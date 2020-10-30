b18242a8f890e5accee55b176d931605c61296f8
parashar-aakash <aakash.parashar@successive.tech>
2020-10-31 02:48:27 +0530

    Feat : Add Files

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -1,13 +1,12 @@
 import * as express from 'express';
 import TraineeController from './controller';
-import validationHandler from '../../libs/routes/validationHandler';
-import validation from './validation';
+// import validationHandler from '../../libs/routes/validationHandler';
 const traineeRoutes = express.Router();
 
 traineeRoutes.route('/')
-        .get( validationHandler( validation.get ) , TraineeController.get )
-        .post( validationHandler( validation.create ) , TraineeController.post )
-        .put( validationHandler( validation.update ) , TraineeController.put )
-        .delete( validationHandler( validation.Delete ) , TraineeController.Delete );
+        .get(  TraineeController.get )
+        .post( TraineeController.post )
+        .put(  TraineeController.put )
+        .delete( TraineeController.Delete );
 
 export default traineeRoutes;
