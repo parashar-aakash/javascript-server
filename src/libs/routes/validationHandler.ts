031069a74d05e3764d4ec5bff21649897a55d417
parashar-aakash <aakash.parashar@successive.tech>
2020-11-01 19:22:24 +0530

    Feat : Add Files

--- src/libs/routes/validationHandler.ts
+++ src/libs/routes/validationHandler.ts
@@ -1,8 +1,26 @@
-export default function(config){
-    return function ( req , res , next ){
-        const b = ( config.id.in[0] ) ; 
-        console.log(typeof(b));
-        // console.log(typeof(body));
-        console.log('config is ' , req.body);
+const validation = ( config ) => { ( req , res , next ) => {
+        console.log(Object.keys(config));
+        console.log(typeof(Object.keys(config)));
+        console.log(Object.keys(req.body));
+
+        console.log('body is' , Object.keys(req.body).length);
+        console.log('query',Object.keys(req.query).length);
+        // console.log();
+        const a = ( config.name.in[0] === 'body' );
+        const b = check( req.body );
+        if( a && b ) {
+            console.log( 'working' );
+        }
+        else{
+            console.log('Not Validated');
+        }
+        next();
     }
+
+}
+
+export default validation;
+
+function check(params) {
+     if(Object.keys(params).length) return false  ;
 }
