403ca621ba025119811c7c760161cb79c6012781
parashar-aakash <akash.parashar@aakash.parashar.successivetech.com>
2020-11-03 18:50:24 +0530

    Feat : Add Files

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -2,12 +2,14 @@ import * as express from 'express';
 import TraineeController from './controller';
 import validationHandler from '../../libs/routes/validationHandler';
 import config from './validation';
+import authMiddleWare from '../../libs/routes/authMiddleWare';
+import {permissions,user} from '../../libs/routes/Constants';
 const traineeRoutes = express.Router();
 
 traineeRoutes.route('/')
-        .get( validationHandler( config.get ) , TraineeController.get )
-        .post( validationHandler( config.create ) , TraineeController.post )
-        .put( validationHandler( config.update ) , TraineeController.put )
-        .delete( validationHandler( config.Delete ) , TraineeController.Delete );
+        .get(authMiddleWare(permissions.getUsers,'read'),validationHandler( config.get ) , TraineeController.get )
+        .post( authMiddleWare(permissions.getUser,'write'),validationHandler( config.create ) , TraineeController.post )
+        .put( authMiddleWare(permissions.getUser,'all'),validationHandler( config.update ) , TraineeController.put )
+        .delete(authMiddleWare(permissions.getUser,'Delete'), validationHandler( config.Delete ) , TraineeController.Delete );
 
 export default traineeRoutes;
