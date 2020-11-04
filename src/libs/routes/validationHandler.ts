2891f4bf5552586c024117ca553c8361380eb836
parashar-aakash <akash.parashar@aakash.parashar.successivetech.com>
2020-11-04 12:53:37 +0530

    Feat : Update File

--- src/libs/routes/validationHandler.ts
+++ src/libs/routes/validationHandler.ts
@@ -2,33 +2,28 @@
 import { NextFunction, Request, Response } from 'express';
 
 
-export default ( config ) => ( req: Request, res: Response, next: NextFunction  ) => {
+export default (config) => (req: Request, res: Response, next: NextFunction) => {
     const errors = [];
-    console.log( 'Inside ValidationHandler Middleware' );
-    console.log( req.body );
-    console.log( req.query );
-    console.log(Object.keys( req.query ).length );
-    const keys = Object.keys( config );
+    console.log('Inside ValidationHandler Middleware');
+    const keys = Object.keys(config);
     keys.forEach((key) => {
         const obj = config[key];
-        console.log('key is' , key);
-        const values = obj.in.map( ( val ) => {
-            return req[ val ][ key ];
+        const values = obj.in.map((val) => {
+            return req[val][key];
         });
         // Checking for In i.e Body or Query
-        if (Object.keys( req[obj.in] ).length === 0) {
+        if (Object.keys(req[obj.in]).length === 0) {
             errors.push({
-                key: {key},
+                key: { key },
                 location: obj.in,
                 message: obj.errorMessage || `Values should be passed through ${obj.in}`,
             });
         }
         // Checking for required
-        console.log('values is' , values);
         if (obj.required) {
             if (isNull(values[0])) {
                 errors.push({
-                    key: {key},
+                    key: { key },
                     location: obj.in,
                     message: obj.errorMessage || `${key} is required`,
                 });
@@ -36,9 +31,9 @@ export default ( config ) => ( req: Request, res: Response, next: NextFunction  
         }
         // Checking for string
         if (obj.string) {
-            if ( !( typeof ( values[0] ) === 'string' ) ) {
+            if (!(typeof (values[0]) === 'string')) {
                 errors.push({
-                    key: {key},
+                    key: { key },
                     location: obj.in,
                     message: obj.errorMessage || `${key} Should be a String`,
                 });
@@ -46,9 +41,9 @@ export default ( config ) => ( req: Request, res: Response, next: NextFunction  
         }
         // Checking for object
         if (obj.isObject) {
-            if ( !( typeof ( values ) === 'object' ) ) {
+            if (!(typeof (values) === 'object')) {
                 errors.push({
-                    key: {key},
+                    key: { key },
                     location: obj.in,
                     message: obj.errorMessage || `${key} Should be an object`,
                 });
@@ -59,9 +54,9 @@ export default ( config ) => ( req: Request, res: Response, next: NextFunction  
             const regex = obj.regex;
             if (!regex.test(values[0])) {
                 errors.push({
-                    key: {key},
+                    key: { key },
                     location: obj.in,
-                    message: obj.errorMessage || `${key} is not valid expression` ,
+                    message: obj.errorMessage || `${key} is not a valid expression`,
                 });
             }
         }
@@ -75,15 +70,15 @@ export default ( config ) => ( req: Request, res: Response, next: NextFunction  
         if (obj.number) {
             if (isNaN(values[0]) || values[0] === undefined) {
                 errors.push({
-                    key: {key},
+                    key: { key },
                     location: obj.in,
-                    message: obj.errorMessage || `${key}  must be an number` ,
+                    message: obj.errorMessage || `${key}  must be a number`,
                 });
             }
         }
     });
     if (errors.length > 0) {
-        res.status(400).send({ errors});
+        res.status(400).send({ errors });
     }
     else {
         next();
@@ -92,7 +87,7 @@ export default ( config ) => ( req: Request, res: Response, next: NextFunction  
 
 
 
-function isNull( obj ) {
-    const a = ( obj === undefined || obj === null );
+function isNull(obj) {
+    const a = (obj === undefined || obj === null);
     return a;
-  }
+}
